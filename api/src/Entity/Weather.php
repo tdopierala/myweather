1d616373d35fcbb25d0bd388c78dc638311aafa0
Tomasz Dopierala <tomasz@dopierala.net.pl>
2019-05-15 20:30:03 +0100

    Adding API application

---
+++ api/src/Entity/Weather.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="App\Repository\WeatherRepository")
+ * @ORM\Table(name="weather")
+ */
+class Weather 
+{
+	/**
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	public $id;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $cityid;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 */
+	public $cityname;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 */
+	public $latitude;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 */
+	public $longitude;
+
+	/**
+	 * @ORM\Column(type="float")
+	 */
+	public $tempavg;
+
+	/**
+	 * @ORM\Column(type="float")
+	 */
+	public $tempmin;
+
+	/**
+	 * @ORM\Column(type="float")
+	 */
+	public $tempmax;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $clouds;
+
+	/**
+	 * @ORM\Column(type="float")
+	 */
+	public $windspeed;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $winddeg;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 */
+	public $description;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $condid;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	public $dt;
+
+
+	public function getCityid()
+	{
+		return $this->cityid;
+	}
+
+	public function setCityid($attr)
+	{
+		$this->cityid = $attr;
+	}
+
+	public function getCityname()
+	{
+		return $this->cityname;
+	}
+
+	public function setCityname($attr)
+	{
+		$this->cityname = $attr;
+	}
+
+	public function getLatitude()
+	{
+		return $this->latitude;
+	}
+
+	public function setLatitude($attr)
+	{
+		$this->latitude = $attr;
+	}
+
+	public function getLongitude()
+	{
+		return $this->longitude;
+	}
+
+	public function setLongitude($attr)
+	{
+		$this->longitude = $attr;
+	}
+
+	public function getTempavg()
+	{
+		return $this->tempavg;
+	}
+
+	public function setTempavg($attr)
+	{
+		$this->tempavg = $attr;
+	}
+
+	public function getTempmin()
+	{
+		return $this->tempmin;
+	}
+
+	public function setTempmin($attr)
+	{
+		$this->tempmin = $attr;
+	}
+
+	public function getTempmax()
+	{
+		return $this->tempmax;
+	}
+
+	public function setTempmax($attr)
+	{
+		$this->tempmax = $attr;
+	}
+
+	public function getClouds()
+	{
+		return $this->clouds;
+	}
+
+	public function setClouds($attr)
+	{
+		$this->clouds = $attr;
+	}
+
+	public function getWindspeed()
+	{
+		return $this->windspeed;
+	}
+
+	public function setWindspeed($attr)
+	{
+		$this->windspeed = $attr;
+	}
+
+	public function getWinddeg()
+	{
+		return $this->winddeg;
+	}
+
+	public function setWinddeg($attr)
+	{
+		$this->winddeg = $attr;
+	}
+
+	public function getDescription()
+	{
+		return $this->description;
+	}
+	public function setDescription($description)
+	{
+		$this->description = $description;
+	}
+
+	public function getCondid()
+	{
+		return $this->condid;
+	}
+
+	public function setCondid($attr)
+	{
+		$this->condid = $attr;
+	}
+
+	public function getDt()
+	{
+		return $this->dt;
+	}
+
+	public function setDt($attr)
+	{
+		$this->dt = $attr;
+	}
+}
---
+++ api/src/Repository/WeatherRepository.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class WeatherRepository extends EntityRepository
+{
+	public function countAll(): array
+	{
+		return $this->getEntityManager()
+			->createQuery('SELECT count(w.id) AS _count FROM App:Weather w')
+			->getResult();
+	}
+
+	public function findTopLocations(): array
+	{
+		return $this->getEntityManager()
+			->createQuery('SELECT count(w.cityid) AS _count, w.cityname AS _cityname FROM App:Weather w GROUP BY w.cityid ORDER BY _count DESC')
+			->setMaxResults(5)
+			->getResult();
+	}
+
+	public function findRecentSearches(): array
+	{
+		return $this->getEntityManager()
+			->createQuery('SELECT w.cityname, w.latitude, w.longitude, w.tempavg, w.clouds, w.windspeed, w.winddeg, w.description, w.dt 
+							FROM App:Weather w ORDER BY w.id DESC')
+			->setMaxResults(10)
+			->getResult();
+	}
+
+	public function findAllTemperatures(): array
+	{
+		return $this->getEntityManager()
+			->createQuery('SELECT min(w.tempavg) as _min, max(w.tempavg) as _max, avg(w.tempavg) as _avg FROM App:Weather w')
+			->getResult();
+	}
+}
