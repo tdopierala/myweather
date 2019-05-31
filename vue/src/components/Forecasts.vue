0c7b1b6b61113990ad0617de5931a257269f0673
tdopierala <tomasz@dopierala.net.pl>
2019-05-31 23:53:04 +0200

    Adding WeatherMap components

---
+++ vue/src/components/Forecasts.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="forecasts-wrapper">
+
+		<SearchBox />
+
+		<DailyVideos />
+
+		<LocationWeather />
+
+		<WeatherInfo />
+
+	</div>
+</template>
+
+<script>
+import SearchBox from '@/components/SearchBox.vue';
+import DailyVideos from '@/components/DailyVideos.vue';
+import LocationWeather from '@/components/LocationWeather.vue';
+import WeatherInfo from '@/components/WeatherInfo.vue';
+
+export default {
+	name: 'Forecasts',
+	components: {
+		SearchBox,
+		DailyVideos,
+		LocationWeather,
+		WeatherInfo,
+	},
+	data() {
+		return {
+
+		};
+	},
+	created() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				};
+
+				console.log(pos);
+				// gmap.map.setCenter(pos);
+			});
+		} else {
+			console.log('Geolocation is not supported by this browser.');
+		}
+	},
+};
+</script>
---
+++ vue/src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="map-wrapper">
+		<WeatherMap />
+		<!-- <BlockUI>
+			<div class="lds-facebook tblack"><div></div><div></div><div></div></div>
+		</BlockUI> -->
+	</div>
+</template>
+
+<script>
+import WeatherMap from '@/components/WeatherMap.vue';
+
+export default {
+	name: 'Map',
+	components: {
+		WeatherMap,
+	},
+	data() {
+		return {
+			message: 'Test message',
+		};
+	},
+};
+</script>
+
+<style>
+
+</style>
