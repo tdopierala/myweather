63d21011dd724fa67b0c8e1e6869b16902baaefb
tdopierala <tomasz@dopierala.net.pl>
2019-06-04 13:31:08 +0200

    Location search by google in SearchBox

--- vue/src/components/SearchBox.vue
+++ vue/src/components/SearchBox.vue
@@ -6,7 +6,7 @@
 			<div class="row">
 				<div class="col-12 col-lg-6">
 					<form>
-						<input type="text" name="search" placeholder="Search location">
+						<input type="text" name="search" id="locSearch" placeholder="Search location">
 						<a href="#">
 							<!-- <i class="fa fa-search"></i> <i class="fas fa-search"></i> -->
 							<font-awesome-icon icon="search" />
@@ -26,7 +26,66 @@
 </template>
 
 <script>
+import gmapsInit from '@/utils/gmaps';
+
 export default {
 	name: 'SearchBox',
+	data() {
+		return {
+			google: null,
+			googleHolder: null,
+			searchInput: null,
+			searchBox: null,
+		};
+	},
+	methods: {
+
+	},
+	/* computed: {
+		google: {
+			set: (val) => {
+				console.log('google setter');
+				this.googleHolder = val;
+			},
+			get: () => {
+				console.log('google getter');
+				return this.googleHolder;
+			},
+		},
+	}, */
+	created() {
+		console.log('created');
+		/* const search = document.getElementById('locSearch');
+		console.log(search);
+		this.searchInput = search; */
+
+		// const searchBox = new this.google.maps.places.SearchBox(searchInput);
+	},
+	async mounted() {
+		console.log('mounted');
+
+		try {
+			const search = document.getElementById('locSearch');
+			console.log(search);
+			this.searchInput = search;
+
+			this.google = await gmapsInit();
+			// console.log(this.google);
+			this.searchBox = new this.google.maps.places.SearchBox(this.searchInput);
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
+		}
+	},
+	watch: {
+		google: {
+			immediate: true,
+			handler() {
+				// console.log('watch google');
+				// console.log(this.searchInput);
+				// this.searchBox = new this.google.maps.places.SearchBox(this.searchInput);
+			},
+		},
+	},
 };
 </script>
